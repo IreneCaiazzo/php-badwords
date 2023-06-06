f9d75f157b40122a0dcaa0ab3d9bc9d12b3036f6
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-06-06 21:12:55 +0200

    censored word

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+$word = $_GET['word'];
+$text = $_GET['message'];
+$censorWord = str_replace($word, '***', $text);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +11,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Bad Words</title>
 </head>
 
 <body>
-
+    <p>Il testo che hai scritto è: <?= $text; ?> </p>
+    <p>E ha <?= strlen($text); ?> caratteri</p>
+    <p>Il testo con la parola censurata è: <?= $censorWord; ?> </p>
+    <p>E ha <?= strlen($censorWord); ?> caratteri </p>
 </body>
 
 </html>
